9833cf9922cc7957515c7fd676a6730630e71409
Luiz <123456789.github@gmail.com>
2024-09-02 22:02:17 -0300

    inicial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sorteio-loteria
- 
+
+Esse código é composto apenas por **uma função** gerar.
+
+Na função é passado dois parametros o
+
+* Primeiro parametro define a quantidae de possibilidades
+
+* Segundo parametro define quantos números serão sorteados 
---
+++ script.js
@@ -0,0 +1,15 @@
+function gerar(totalPossibilidades, numerosSorteados) {
+    let sorteio = [];
+    while (sorteio.length != numerosSorteados) {
+      let numeroSorteado = parseInt(Math.random() * totalPossibilidades) + 1;
+      if (sorteio.includes(numeroSorteado) != true) {
+        sorteio.push(numeroSorteado);
+      }
+  
+      if (sorteio.length == numerosSorteados) {
+        console.log(sorteio.sort());
+      }
+    }
+  }
+  
+  gerar(80, 6);
