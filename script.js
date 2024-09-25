5e68ab723a97cb8f90a7523daf4c8e250991dc4c
Luiz <123456789.github@gmail.com>
2024-09-25 11:55:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function gerar(totalPossibilidades, numerosSorteados) {
-    let sorteio = [];
-    while (sorteio.length != numerosSorteados) {
       let numeroSorteado = parseInt(Math.random() * totalPossibilidades) + 1;
-      if (sorteio.includes(numeroSorteado) != true) {
-        sorteio.push(numeroSorteado);
-      }
-  
-      if (sorteio.length == numerosSorteados) {
-        console.log(sorteio.sort());
-      }
+  let sorteio = [];
+  while (sorteio.length != numerosSorteados) {
+    let numeroSorteado = parseInt(Math.random() * totalPossibilidades) + 1;
+    if (sorteio.includes(numeroSorteado) != true) {
+      sorteio.push(numeroSorteado);
+    }
+
+    if (sorteio.length == numerosSorteados) {
+      console.log(sorteio.sort());
     }
   }
-  
-  gerar(80, 6);
+}
+
+gerar(80, 6);
